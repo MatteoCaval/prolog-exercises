15cf0c768a39f2b24e092314935c7b57797bc555
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-09-02 19:26:02 +0200

    seqR

--- redo_lab_1/lab1.pl
+++ redo_lab_1/lab1.pl
@@ -122,6 +122,12 @@ sublist([X|Xs], L) :- sublist(Xs, L), search(X, L).
 seq(0,[]).
 seq(N,[0|T]):- N > 0, N2 is N-1, seq(N2,T).
 
+% seqR(N,List)
+% example: seqR(4,[4,3,2,1,0]).
+
+seqR(0, [0]).
+seqR(N, [N|T]) :- N > 0, N2 is N-1, seqR(N2,T).
+
 
 
 
