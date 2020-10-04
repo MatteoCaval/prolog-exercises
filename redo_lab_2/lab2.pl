325b10ce6c1f6f44ea5a94258fbad24cfd381765
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-10-04 15:46:02 +0200

    fromCircList

--- redo_lab_2/lab2.pl
+++ redo_lab_2/lab2.pl
@@ -15,11 +15,48 @@ left(B,B2) :- right(B2,B).
 %current(+Bilist, -Elem)
 current(bilist([H|_], _), H).
 
-
-mynot(P) :- P, !, myfail.
-myfail :- 1=2.
+% implementazione del predicato not
+mynot(P) :- P, !, fail.
+mynot(P). % arrivo qui se P non ha avuto esisto positivo 
 
 
 % dropAny(?Elem,?List,?OutList)
 dropAny(X,[X|T],T).
 dropAny(X,[H|Xs],[H|L]):-dropAny(X,Xs,L).
+
+% dropAny(10,[10,20,10,30,10],L).
+
+
+% fromList(+List,-Graph)
+fromList([_],[]).
+fromList([H1,H2|T],[e(H1,H2)|L]):- fromList([H2|T],L).
+
+% fromList([10,20,30],[e(10,20),e(20,30)]).
+% fromList([10,20],[e(10,20)]).
+% fromList([10],[]).
+
+% fromCircList(+List,-Graph)
+
+fromCircList([],[]).
+fromCircList([H1|T],L) :- fromCircList([H1|T],L,H1). 
+fromCircList([E],[e(E,H1)],H1).
+fromCircList([H1,H2|T],[e(H1,H2)|L],F) :- fromCircList([H2|T],L,F).
+
+% fromCircList([10,20,30],[e(10,20),e(20,30),e(30,10)]).
+% fromCircList([10,20],[e(10,20),e(20,10)]).
+% fromCircList([10],[e(10,10)]).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
